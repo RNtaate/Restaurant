3e3ddfc85060ca896e31157f016aa59cf6cea57b
RNtaate <royhyde14@gmail.com>
2020-12-13 15:11:52 +0300

    Create nav bar, add buttons to it, append it to body element

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import loadInitialPage from './script1';
 
+let body = document.querySelector('body');
+
 let divContent = document.getElementById('content');
 let title = 'NORP RESTAURANT';
 let myCaption = 'Your stop for an exquisite and relaxing meal.'
 divContent.appendChild(loadInitialPage(title, myCaption));
+
+let nav = document.createElement('nav');
+
+let tabButton = (content, firstClassName = '') => {
+  let myButton = document.createElement('button');
+  myButton.textContent = content;
+  myButton.classList.add(firstClassName);
+  return myButton;
+}
+
+nav.appendChild(tabButton('Home', 'tab-button'));
+nav.appendChild(tabButton('Menu', 'tab-button'));
+nav.appendChild(tabButton('Contacts', 'tab-button'));
+
+body.appendChild(nav);
